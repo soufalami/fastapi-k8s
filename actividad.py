7b188a8e137f69c56a651de1b17bf8a7edc726d4
Soufiane <89970849+soufalami@users.noreply.github.com>
2024-04-08 19:39:14 +0200

    Create actividad.py

---
+++ actividad.py
@@ -0,0 +1,22 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("Spark Connect Example") \
+    .master("k8s://https://kubernetes.default.svc.cluster.local") \
+    .config("spark.kubernetes.namespace", "virtualizacion") \
+    .config("spark.kubernetes.authenticate.driver.serviceAccountName", "spark") \
+    .config("spark.executor.instances", "2") \
+    .config("spark.kubernetes.container.image", "spark:3.5.1") \
+    .config("spark.driver.host", "spark-driver-headless") \
+    .config("spark.driver.bindAddress", "0.0.0.0") \
+    .config("spark.driver.blockManager.port", "7777") \
+    .config("spark.driver.port", "2222") \
+    .getOrCreate()
+
+resultados = [random.randint(1, 6) for _ in range(6)]
+
+
+df = spark.createDataFrame([(r,) for r in resultados], ["resultado"])
+df.show()
+
+spark.stop()
